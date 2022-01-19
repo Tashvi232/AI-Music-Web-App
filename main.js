c30dfafbc3534eb59b8859087d4674f97594595b
Tashvi232 <79177478+Tashvi232@users.noreply.github.com>
2022-01-19 19:44:50 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+rightWristX="";
+rightWristY="";
+leftWristX="";
+leftWristY="";
 
 function preload() {
     song1=loadSound("Kevin MacLeod - Fluffing a Duck.mp3");
@@ -11,8 +15,20 @@ function setup() {
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Model is loaded");
 }
 
 function draw() {
     image(video,0,0,600,500);
+}
+
+function gotPoses(results) {
+    if(results.length>0) {
+        console.log(results);
+    }
 }
