896359881a1d0307e3b7f5ab12e9a31b42c34a1d
Tashvi232 <79177478+Tashvi232@users.noreply.github.com>
2023-08-18 14:51:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ rightWristX="";
 rightWristY="";
 leftWristX="";
 leftWristY="";
-score="";
+left_score="";
 song_status="";
+right_score="";
+Song_status="";
 
 function preload() {
     song=loadSound("Kevin MacLeod - Fluffing a Duck.mp3");
@@ -29,14 +31,23 @@ function draw() {
     image(video,600,500,0,0);
     fill(255, 0, 0);
     stroke(0);
-    song_status= song.isPlaying()
+    song_status= song.isPlaying();
+    Song_status=Song.isPlaying();
     if (leftWrist.score > 0.2) {
         circle(leftWristX,leftWristY,30);
         Song.stop();
     }
+    if (rightWristScore > 0.2) {
+        circle(leftWristX,leftWristY,30);
+        song.stop();
+    }
     if(song_status == false) {
-        song.play()
+        song.play();
         document.getElementById("song").innerHTML = "Fluffing a Duck";
+    }
+    if(Song_status == false) {
+        Song.play();
+        document.getElementById("song").innerHTML = "Silly Intro";
     }
 }
 
@@ -47,6 +58,7 @@ function gotPoses(results) {
         rightWristX = pose.rightWrist.x;
         rightWristY = pose.rightWrist.y;
         results[0].pose.keypoints[9].score;
+        results[0].pose.keypoints[10].score;
         console.log(results);
     }
 }
